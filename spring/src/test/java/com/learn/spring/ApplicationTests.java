148f1956dbd9ac384aafcd8523d152645ce99d84
soundaramp <p.soundaram@gmail.com>
2025-08-02 11:28:35 +0530

    new project with log implementation

--- spring/src/test/java/com/learn/spring/ApplicationTests.java
+++ spring/src/test/java/com/learn/spring/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Learning.SpringBootHandsOn;
+package com.learn.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootHandsOnApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
