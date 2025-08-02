034c03b83aacf7ce3a6b4031d729dc54c1efc14a
soundaramp <p.soundaram@gmail.com>
2025-08-02 09:34:17 +0530

    spring boot project

---
+++ SpringBootHandsOn/src/test/java/com/example/Learning/SpringBootHandsOn/SpringBootHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Learning.SpringBootHandsOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
